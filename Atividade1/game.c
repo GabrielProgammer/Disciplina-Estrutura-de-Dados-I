bf5085ed3397c8f250a359ccc3fc9ffd7ec04a80
rhuan.marx666 <rhuan.marx666@gmail.com>
2016-09-20 09:46:42 -0300

    modifiquei o'games.h'

--- Atividade1/game.c
+++ Atividade1/game.c
@@ -1,35 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "games.h"
-
-void cria_games(Games *p, char nomeP, char tipoP, int anoP, int downloadasP, float notaP){
-int i, j;
+int i,j;
+Games  temp;
+char escolhe_tipo[100];
+float notaX;
+void cria_games(Games *p, char nome, char tipo, int ano, int download, float nota){
 for (i = 0; i <=100; i++)
 	{
-		imprime_linha();
 		printf("\nCADASTRANDO %d JOGO!\n", i+1);
-		imprime_linha();
 		printf("\n\n");
 		printf("Insira o nome do %d jogo\n",i+1);
-		scanf ("%s",p->nome=nomeP);
+		fgets (p->nome, 100, stdin);
 		printf("Insira o tipo do %d jogo\n", i+1);
-		scanf ("%s", p->tipo=tipoP);
-		printf("Insira o ano de lancamento: \n");
-		scanf("%d", p->ano=anoP);
-		printf("Insira o numero de downloads: \n");
-		scanf("%d", p->download=downloadP);
-		printf("Insira a nota do jogo %d\n", i+1);
-		scanf("%f", p->nota=notaP);
+		fgets (p->tipo, 100, stdin);
+		printf("Insira o ano de lancamento: %d\n",p->ano);
+		printf("Insira o numero de downloads: %d\n",p->download);
+		printf("Insira a nota do jogo %f\n", p->nota);
 		printf("\n");
 		getchar();
 	}
-	imprime_linha();
+	
 }
-}
+
 void mostra_games(Games *a){
 int i,j;
+Games p[100], temp;
 printf("\nListando todos os jogos!\n");
-	imprime_linha();
 	printf("\n\n");
 
 	for (i = 0; i <=100; i++)
@@ -37,30 +34,49 @@ printf("\nListando todos os jogos!\n");
 		printf("Nome: %s", a->nome);
 		printf("Tipo: %s", a->tipo);
 		printf("Ano: %d\n", a->ano);
-		printf("Dowloads: %d\n", a->downloads);
+		printf("Dowloads: %d\n", a->download);
 		printf("Nota: %.1f\n", a->nota);
-		imprime_linha();
 		printf("\n\n");
+	}
+
+
+Games *ordena_rating_crescente (Games *a){
+for (i = 0; i <=100; i++)
+	{
+		
+	printf("Ordenando notas por ordem crescente!\n");
+	printf("\n\n");
+	
+	for(i=0; i <=100; i++)
+	{
+		for(j = 0;j <=100; j++)
+		{
+			if(a->nota > a[j+1].nota)
+			{
+				temp = a[j];
+				a[j] = a[j+1];
+				a[j+1] = temp;
+			}
+		}	
 	}
 }
 
 }
-void avaliacao(Games *a, char tipoP){
-int i,j;
-for (i = 0; i < =100; i++)
+void exibe_rating_crescente (Games *a){
+for (i = 0; i < 100; i++)
 	{
 		printf("Nome: %s", a->nome);
 		printf("Tipo: %s", a->tipo);
 		printf("Ano: %d\n", a->ano);
-		printf("Dowloads: %d\n", a->downloads);
+		printf("Dowloads: %d\n", a->download);
 		printf("Nota: %.1f\n", a->nota);
-		imprime_linha();
 		printf("\n\n");
 	}
 }
 
-{
-	for(i = 100-1; i>= 1; i--)
+}
+Games *ordena_rating_decrescente (Games *a){
+for(i = 100; i>= 1; i--)
 	{
 		for(j=0; j<i ; j++)
 		{
@@ -72,25 +88,21 @@ for (i = 0; i < =100; i++)
 			}
 		}	
 	}
+}
 
-
-
-{
-	for(i = 0; i < =100; i++)
+void *exibe_rating_decrescente(Games *a){
+for(i = 0; i <=100; i++)
 	{
 		printf("Nome: %s", a->nome);
 		printf("Tipo: %s", a->tipo);
 		printf("Ano: %d\n", a->ano);
-		printf("Dowloads: %d\n", a->downloads);
+		printf("Dowloads: %d\n", a->download);
 		printf("Nota: %.1f\n", a->nota);
-		imprime_linha();
 		printf("\n\n");	
 	}
 }
 
-}
-void estilo(Games *a, char tipo){
-int i,j;
+Games *ordena_tipo (Games *a){
 for (i = 0; i <=100; i++)
 	{
 		if (!strcmp(escolhe_tipo, a->tipo))
@@ -98,18 +110,30 @@ for (i = 0; i <=100; i++)
 			printf("Nome: %s", a->nome);
 			printf("Tipo: %s", a->tipo);
 			printf("Ano: %d\n", a->ano);
-			printf("Dowloads: %d\n", a->downloads);
-			printf("Nota: %.1f\n", a->rating);
-			imprime_linha();
+			printf("Dowloads: %d\n", a->download);
+			printf("Nota: %.1f\n", a->nota);
 			printf("\n\n");
 		}
 	}
 }
-void lancamento(Games *a, int ano){
-int i,j;
-imprime_linha();
-	printf("\nListando em ordem crescente!\n");
-	imprime_linha();
+Games *ordena_ano_x (Games *a){
+printf("\nMostrando jogos apenas com a nota %.1f\n", notaX);
+	printf("\n\n");
+	for (i = 0; i < 100; i++)
+	{
+		if (notaX == a->nota)
+		{
+			printf("Nome: %s", a->nome);
+			printf("Tipo: %s", a->tipo);
+			printf("Ano: %d\n", a->ano);
+			printf("Dowloads: %d\n", a->download);
+			printf("Nota: %.1f\n", a->nota);
+			printf("\n\n");
+		}
+	}	
+}
+Games *ordena_ano_crescente (Games *a){
+printf("\nListando em ordem crescente!\n");
 	printf("\n\n");
 	
 	for (i = 0; i <=100; i++)
@@ -124,26 +148,20 @@ imprime_linha();
 			}
 		}
 	}
-			
-	for (i = 0; i < =100; i++)
+	for (i = 0; i <=100; i++)
 	{
 		printf("Nome: %s", a->nome);
 		printf("Tipo: %s", a->tipo);
 		printf("Ano: %d\n", a->ano);
-		printf("Dowloads: %d\n", a->downloads);
+		printf("Dowloads: %d\n", a->download);
 		printf("Nota: %.1f\n", a->nota);
-		imprime_linha();
 		printf("\n\n");
 	}
 }
-
-
-	imprime_linha();
-	printf("\nListando em ordem decrescente!\n");
-	imprime_linha();
-	printf("\n\n");
-	
-	for(i = TAM-1; i>= 1; i--)
+Games *ordena_ano_decrescente(Games *a){
+printf("\nListando em ordem decrescente!\n");
+printf("\n\n");
+	for(i = 100; i>= 1; i--)
 	{
 		for(j=0; j<i; j++)
 		{
@@ -156,18 +174,16 @@ imprime_linha();
 		}	
 	}
 	
-	for (i = 0; i < =100; i++)
+	for (i = 0; i <=100; i++)
 	{
 		printf("Nome: %s", a->nome);
 		printf("Tipo: %s", a->tipo);
 		printf("Ano: %d\n", a->ano);
-		printf("Dowloads: %d\n", a->downloads);
+		printf("Dowloads: %d\n", a->download);
 		printf("Nota: %.1f\n", a->nota);
-		imprime_linha();
 		printf("\n\n");
 	}
 }
-}
-void alfabetica(Games *a, char nome){
 
-}
+
+
