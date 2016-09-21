cb177bc972449c4baa9b1be45bf4867853ce29b4
fgf <layla-poo@hotmail.comgit config  user.email layla-poo@hotmail.com>
2016-09-21 11:08:02 -0300

    modificando tad

--- Atividade2/layla/atividadeT.c
+++ Atividade2/layla/atividadeT.c
@@ -2,18 +2,32 @@
 #include<stdlib.h>
 #include "porta.h"
 
-void criaporta(Porta *p,char valorC,char valort1, float valort2,float valorp){
+void criaporta(Porta *p,char valorC,char valort1, float valort2,float valorp,){
 p->cor=valorC;
 p->tipo=valort1;
 p->tamanho=valort2;
 p->preço=valorp;
+p->largura=valorL;
+p->design=valorD;
+p->desgaste=valord;
+p->madeira=valorM;
 
-void valorC(Porta *a,char valorC){
+void vervalorC(Porta *a,char valorC){
   for(i=0;i<3;i++){
   a->cor=valorC;
+  printf("a cor da porta
  }
 }
 
-void valort1(Porta *a,char valort1){
+void vervalort1(Porta *a,char valort1){
+ for(i=0;i<3;i++){
+ a->tipo=valort1;
+ printf("o tipo e: %c",valort1);
+ }
+}
 
+void vervalort2(Porta *a,float valort2){
+  for(i=0;i<3;i++){
+  a->tamanho=valort2
+  printf("o tamanho e :%f ",valort2);
 
--- Atividade2/layla/atividadeT.h
+++ Atividade2/layla/atividadeT.h
@@ -1,14 +1,17 @@
 typedef struct porta{
-char cor,tipo;
-float preço,tamanho;
+char cor,tipo,madeira,design;
+float preço,tamanho,largura,desgaste;
 }Porta;
 
 //operações
-void criaporta(Porta *p,char cor,float tamanho,float preço,char tipo);
-void escolhetipo(Porta *a,char tipo);
-void escolhecor(Porta *a,char cor);
-void escolhetamanho(Porta *a,float tamanho);
-void escolhepreço(Porta *a,float preço);
-
+void criaporta(Porta *p,char cor,float tamanho,float preço,char tipo,char design,float largura,float desgaste);
+void vertipo(Porta *a,char tipo);
+void vercor(Porta *a,char cor);
+void vertamanho(Porta *a,float tamanho);
+void verpreço(Porta *a,float preço);
+void verlargura(Porta *a,float largura);
+void vertipodemadeira(Porta *a,char madeira);
+void verdesign(Porta *a,char design);
+void verdesgaste(Porta *a,float desgaste);
 void vervalores(Porta *a);
 
