23f7a0e9c8224b1b3117edc427db83bb21f7b405
fgf <layla-poo@hotmail.comgit config  user.email layla-poo@hotmail.com>
2016-09-22 10:50:59 -0300

    modificando meu tad

---
+++ Atividade2/amandacliente.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include"amanda.h"
+
+int main  () {
+	Ponto a, m=&a
+	void verdimensoes(m,15,22.4);
+        void areadagaveta(m,15,22.4);
+        void espasolivre(m, 336 , 112);
+        void espasoocupado(m,336,224  );
+        void desgatedagaveta (m,15,22.4);
+        void movimentadagaveta(m,0);
+        void repolso(m, 0);
+        void objetoinserido(m, 2, 3 );
+}
