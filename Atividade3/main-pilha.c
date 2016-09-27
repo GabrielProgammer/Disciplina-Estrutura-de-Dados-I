09d7c014e6bca5eb87adeab4e852189d67d6029d
rhuan.marx666 <rhuan.marx666@gmail.com>
2016-09-27 08:07:08 -0300

    adicionando 'main-pilha.c'para ser implementada'

--- Atividade3/main-pilha.c
+++ Atividade3/main-pilha.c
@@ -1,25 +1,42 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "pilha.h"
 
-int main()
-{
-	Pilha p, *a = &p;
-	inicializa(a);
-	retira(a);
-	insere(a, 14);
-	insere(a, 5);
-	insere(a, 21);
-	imprime_topo(a);
-	insere(a, 4);
-	insere(a, 7);
-	insere(a, 40);
-	imprime_topo(a);
-	imprime_multiplo7(a);
-	imprime_pilha(a);
-	retira(a);
-	retira(a);
-	imprime_pilha(a);
-	imprime_multiplo7(a);
+int main(){
+Pilha p, *a = &p;
+ 	inicializa(a);
+ 	insere(a, 1);
+ 	retira(a);
+ 	insere(a, 14);
+ 	insere(a, 5);
+ 	insere(a, 21);
+ 	imprime_topo(a);
+ 	insere(a, 4);
+ 	insere(a, 7);
+ 	insere(a, 8);
+ 	insere(a, 11);
+ 	insere(a, 40);
+ 	imprime_topo(a);
+	
+ 	imprime_multiplo7(a);
+ 	imprime_pilha(a);
+ 	retira(a);
+ 	retira(a);
+ 	imprime_topo(a);
+ 
+ 
+ 	imprime_pilha(a);
+ 	imprime_multiplo7(a);
+
+
+inverte(a);
+imprime_pilha(a);
+     
+ 	
+
+ 	
+
+
+
 }
 
