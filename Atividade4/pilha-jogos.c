3b51294de0ceb5729966d56644954ae29760ddb2
Amanda Grazielly De Sousa Gama <grazyamandaa@gmail.com>
2016-09-27 11:34:11 -0300

    modifiquei o .h e implementei visualização de jogos e imprime o ultimo jogo

--- Atividade4/pilha-jogos.c
+++ Atividade4/pilha-jogos.c
@@ -2,17 +2,43 @@
 #include <sdlib.h>
 #include "pilha-jogos.h"
 
+
 void inicializa(Pilha *p){
+
+p->topo +1;
 }
-void vizualização_jogos(Game *p, char nome[TAM], char tipo[TAM], int ano, int download, float nota){
+void vizualização_jogos(Game *p, char auxnome[TAM], char auxtipo[TAM], int auxano, int auxdownload, float auxnota, int i){
+ for (i=0, i<TAM, i++ ){
+ char auxnome=p->nome[TAM];
+ char auxtipo[TAM]=p->tipo[TAM];
+ int auxano=p->ano;
+ int auxdownload=p->download;
+ float auxnota=p->nota;
+ printf("os jogos que foram jogados foram\n");
+ return auxnome;
+ return auxtipo;
+ return auxano;
+ return auxdownload;
+ return auxnota;
+ 
+ 
+
 }
 void insere(Pilha *p, int elemento){
 }
 void retira(Pilha *p){
 }
-void emprime_últimojogo(Pilha *p, int elemento){
+void emprime_últimojogo(Pilha *p ){
+printf(" o último jogo a jogado foi %s \n ", p->Game[p->nome];
+printf("tipo %s \n ",p->Game[p->tipo]);
+printf("ano %d\n", p->Gama[p->ano]);
+printf("download %d\n",p->Game[p->download]);
+printf("nota %f\n",p->Game[p->nota]);
 }
 void removejogoda_base(Pilha *p){
+
+
 }
 void imprime_pilhajogos(Pilha*a){
+
 }
