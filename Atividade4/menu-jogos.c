2cece154dfc621fc8b5d98b5451191bff11f4c0a
rhuan.marx666 <rhuan.marx666@gmail.com>
2016-09-27 10:21:18 -0300

    adicionado o modelos para o TADpilhagames'pilha-jogos.h''pilhas-jogos.c''menu-jogos.c'

---
+++ Atividade4/menu-jogos.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <sdlib.h>
+#include "pilha-jogos.h"
+
+int main(){
+Pilha p, *a = &p;
+}
---
+++ Atividade4/pilha-jogos.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <sdlib.h>
+#include "pilha-jogos.h"
+
+void inicializa(Pilha *p){
+}
+void vizualização_jogos(Game *p, char nome[TAM], char tipo[TAM], int ano, int download, float nota){
+}
+void insere(Pilha *p, int elemento){
+}
+void retira(Pilha *p){
+}
+void emprime_últimojogo(Pilha *p, int elemento){
+}
+void removejogoda_base(Pilha *p){
+}
+void imprime_pilhajogos(Pilha*a){
+}
