823cf3b556c4d7770c6640d60cfeab7c87942a39
rhuan.marx666 <rhuan.marx666@gmail.com>
2016-09-27 08:03:53 -0300

    adicionando 'pilha.h'para ser implementada'

--- Atividade3/pilha.h
+++ Atividade3/pilha.h
@@ -1,14 +1,21 @@
-#define TAMANHO 4
+#define TAMANHO 50
 
-typedef struct pilha
-{
+struct pilha{
+	
 	int pilha[TAMANHO];
 	int topo;
-}Pilha;
+    int valor;  
+};
+
+typedef struct pilha Pilha;
+
 
 void inicializa(Pilha *p);
 void insere(Pilha *p, int elemento);
 void retira(Pilha *p);
-void imprime_topo(Pilha *p);
-void imprime_multiplo7(Pilha *p);
-void imprime_pilha(Pilha *p);
+void emprime_topo(Pilha *p, int elemento);
+void removebase(Pilha *p);
+void remove_meio(Pilha*p, int valor,int topo);
+void inverte(Pilha *p);
+void imprime_mutiplo7(Pilha *p);
+void imprime_pilha(Pilha*a);
