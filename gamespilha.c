45672abd7e669df8be79bdbc9fd0865ee5c03f72
Amanda Grazielly De Sousa Gama <grazyamandaa@gmail.com>
2016-09-29 11:29:08 -0300

    pilha jogos

---
+++ gamespilha.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include "pilhajogo.h"
+
+ void retira (Pilha*p){
+p->jogo=jogo-1
+}
+ void insere(Pilha *p , char njogo){
+  p->jogo=p->jogo+1;
+  p->Pilha[p->jogo]=njogo;
+
+ }
+ void vertopo( Pilha *p){
+ p->jogo
+printf(" o jogo que está no topo é %c \n", p->jogo);
+}
+
